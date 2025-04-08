0eeb1f14f914414a16dc711cd1fedc0ab4fc05a5
Raj <rajshah0192000@gmail.com>
2025-04-07 21:48:33 -0400

    Lombok Library Added

--- src/main/java/com/example/api/controller/JournalEntryController.java
+++ src/main/java/com/example/api/controller/JournalEntryController.java
@@ -2,9 +2,12 @@ package com.example.api.controller;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,43 +35,57 @@ public class JournalEntryController {
 
     
     @GetMapping("/")
-    public List<JournalEntry> getAll(){
-       return journalEntryService.getAll(); 
+    public ResponseEntity<?> getAll(){
+       List<JournalEntry> all = journalEntryService.getAll(); 
+       if(all != null && !all.isEmpty()){
+        return new ResponseEntity<>(all,HttpStatus.OK);
+       }
+       return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping("/")
-    public boolean createEntry(@RequestBody JournalEntry myEntry) {
-        myEntry.setDate((LocalDateTime.now()));
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry) {
+        try{
+            myEntry.setDate((LocalDateTime.now()));
         journalEntryService.saveEntry(myEntry);
-        return true;
+        return new ResponseEntity<>(myEntry,HttpStatus.CREATED);
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
     @GetMapping("/id/{myId}")
-    public JournalEntry getJournalEntryById(@PathVariable ObjectId myId){
-        return journalEntryService.getByID(myId).orElse(null);
+    public ResponseEntity<JournalEntry> getJournalEntryById(@PathVariable ObjectId myId){
+        Optional<JournalEntry> journalEntry = journalEntryService.getByID(myId);
+        if(journalEntry.isPresent()){
+            return new ResponseEntity<>(journalEntry.get(),HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("/id/{myId}")
-    public boolean deleteJournalEntryById(@PathVariable ObjectId myId){
+    public ResponseEntity<?> deleteJournalEntryById(@PathVariable ObjectId myId){
         journalEntryService.deleteByID(myId);
-        return true;
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PutMapping("id/{myId}")
-    public JournalEntry updateJournalEntryById(@PathVariable ObjectId myId, @RequestBody JournalEntry newEntry) {
+    public ResponseEntity<JournalEntry> updateJournalEntryById(@PathVariable ObjectId myId, @RequestBody JournalEntry newEntry) {
         JournalEntry old = journalEntryService.getByID(myId).orElse(null);
         
-        if (old != null) {
-            if (newEntry.getTitle() != null && !newEntry.getTitle().isEmpty()) {
-                old.setTitle(newEntry.getTitle());
-            }
-            if (newEntry.getContent() != null && !newEntry.getContent().isEmpty()) {
-                old.setContent(newEntry.getContent());
+            if (old != null) {
+                if (newEntry.getTitle() != null && !newEntry.getTitle().isEmpty()) {
+                    old.setTitle(newEntry.getTitle());
+                }
+                if (newEntry.getContent() != null && !newEntry.getContent().isEmpty()) {
+                    old.setContent(newEntry.getContent());
+                }
+        
+                journalEntryService.saveEntry(old);
+                return new ResponseEntity<>(old,HttpStatus.OK);
+                
             }
     
-            journalEntryService.saveEntry(old);
-            return old;
-        }
     
-        return null;
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/example/api/entity/JournalEntry.java
+++ src/main/java/com/example/api/entity/JournalEntry.java
@@ -1,43 +1,18 @@
 package com.example.api.entity;
-
-
-
 import java.time.LocalDateTime;
-import java.util.Date;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Data;
+
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
     @Id
     private ObjectId id;
     private String title;
     private String content;
     private LocalDateTime date;
-    public ObjectId getId() {
-        return id;
-    }
-    public LocalDateTime getDate() {
-        return date;
-    }
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public String getContent() {
-        return content;
-    }
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
